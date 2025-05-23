3fb8e905cc5b8bd3457cc7164b5f265900515d59
Gabriel Sánchez <166429581+gabiisnchez@users.noreply.github.com>
2025-05-23 15:11:46 +0200

    Upload Data Base Final Version

---
+++ LaLigaFantasySimulator_DataBase_FinalVersion/laliga_equipos.sql
@@ -0,0 +1,53 @@
+-- MySQL dump 10.13  Distrib 8.0.42, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: laliga
+-- ------------------------------------------------------
+-- Server version	9.3.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `equipos`
+--
+
+DROP TABLE IF EXISTS `equipos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `equipos` (
+  `id_equipo` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(100) NOT NULL,
+  `id_escudo` varchar(45) NOT NULL,
+  `valoración` float NOT NULL,
+  PRIMARY KEY (`id_equipo`,`nombre`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `equipos`
+--
+
+LOCK TABLES `equipos` WRITE;
+/*!40000 ALTER TABLE `equipos` DISABLE KEYS */;
+INSERT INTO `equipos` VALUES (1,'FC BARCELONA','barcelona',5),(2,'REAL MADRID','real_madrid',5),(3,'ATLETICO DE MADRID','atletico',4.5),(4,'ATHLETIC CLUB','athletic',4.5),(5,'VILLAREAL FC','villareal',4),(6,'REAL BETIS','betis',4.5),(7,'RC CELTA','celta',3.5),(8,'RAYO VALLECANO','rayo',3.5),(9,'CA OSASUNA','osasuna',3.5),(10,'RCD MALLORCA','mallorca',3.5),(11,'REAL SOCIEDAD','real_sociedad',4),(12,'VALENCIA FC','valencia',4),(13,'GETAFE FC','getafe',3.5),(14,'DEPORTIVO ALAVES','alaves',3),(15,'GIRONA FC','girona',3.5),(16,'SEVILLA FC','sevilla ',1.5),(17,'RCD ESPANYOL','espanyol',3.5),(18,'CD LEGANES','leganes',3),(19,'UD LAS PALMAS','las_palmas',3),(20,'REAL VALLADOLID CF','valladolid',3);
+/*!40000 ALTER TABLE `equipos` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-05-23 10:17:13
